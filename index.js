df89060d9c330fcd8061184b057ba7f4a58f7c6e
Freddy Jetty Johnson <freddy.jetty.johnson@sap.com>
2021-08-02 23:55:57 +0530

    Added search support for current day

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 
 const {TOKEN, SERVER_URL} = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const COWIN_API = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public';
 const URI = `/webhook/${TOKEN}`;
 const WEBHOOK_URL = SERVER_URL + URI;
 
@@ -17,11 +18,12 @@ const init = async () => {
 };
 
 app.post(URI, async (req, res) => {
-    if (!req.body)  return res.send();
+    if (!(req.body && req.body.message))  return res.send();
+    
     const message = req.body.message;
-
     if (message.entities) {
         const {type, offset, length} = message.entities[0];
+
         if (type === 'bot_command') {
             const text = message.text.substr(offset, length);
             const cmd = text.split(' ')[0];
@@ -30,25 +32,96 @@ app.post(URI, async (req, res) => {
                     chat_id: message.chat.id,
                     text: 'Hi @' + message.chat.username + ', Hope you\'re ready to get vaccinated! Let\'s find you a free slot. To search for a slot, type the command /search and enter your PIN Code. Type /help if you have additional queries!'
                 };
-                const result = await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
+                await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
             } else if (cmd === '/help') {
                 const reply = {
                     chat_id: message.chat.id,
                     text: 'For checking free slots, type /search and enter your PIN Code'
                 };
-                const result = await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
+                await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
             } else if (cmd === '/search') {
                 const query = message.text.substr(text.length + 1);
                 const pincode = query.split(' ')[0];
-                console.log(pincode);
+                if (isNaN(pincode) || pincode.length !== 6) {
+                    const reply = {
+                        chat_id: message.chat.id,
+                        text: 'Please enter a valid 6 digit PIN Code'
+                    };
+                    await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
+                } else {
+                    //API call to cowin
+                    let oDate = new Date();
+                    let sDate = (((oDate.getDate() > 9) ? oDate.getDate() : ('0' + oDate.getDate())) + '-' + ((oDate.getMonth() > 8) ? (oDate.getMonth() + 1) : ('0' + (oDate.getMonth() + 1))) + '-' + oDate.getFullYear());
+                    const res = await axios.get(`${COWIN_API}/findByPin?pincode=${pincode}&date=${sDate}`);
+                    
+                    let sessions = res.data.sessions;
+                    if (!sessions.length) {
+                        const reply = {
+                            chat_id: message.chat.id,
+                            text: 'Sorry, no slots available for today'
+                        };
+                        await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
+                    } else {
+                        let sParsedText = '', reply = {};
+                        sessions = sessions.filter(session => {
+                            return session.available_capacity;
+                        });
+
+                        if (sessions.length) {
+                            sessions.forEach(session => {
+                                if (session.available_capacity) {
+                                    sParsedText += `Vaccine: *${session.vaccine}*\nCentre Name: *${session.name}*\nAge Group: *${session.min_age_limit}*\n`;
+    
+                                    sParsedText += parseInt(session.fee) ? `Cost: *₹${session.fee}*\n` : `Cost: *Free*\n`;
+                                
+                                    if (session.available_capacity_dose1) {
+                                        sParsedText += `Dose 1 slots: *${session.available_capacity_dose1}*\n`;
+                                    }
+                                    if (session.available_capacity_dose2) {
+                                        sParsedText += `Dose 2 slots: *${session.available_capacity_dose2}*\n`;
+                                    }
+                                    reply = {
+                                        chat_id: message.chat.id,
+                                        parse_mode: 'MarkdownV2',
+                                        text: sParsedText,
+                                        reply_markup: {
+                                            inline_keyboard: [
+                                                [
+                                                    {
+                                                        text: 'Book Now!',
+                                                        url: `https://selfregistration.cowin.gov.in/`
+                                                    }
+                                                ]
+                                            ]
+                                        }
+                                    };
+                                    axios.post(`${TELEGRAM_API}/sendMessage`, reply);
+                                    sParsedText = '';
+                                }
+                            });
+                        } else {
+                            reply = {
+                                chat_id: message.chat.id,
+                                text: 'Sorry, no slots available for today'
+                            };
+                            await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
+                        }
+                    }
+                }
             } else {
                 const reply = {
                     chat_id: message.chat.id,
                     text: 'Sorry, I didn\'t understand that. Type /help for more information'
                 };
-                const result = await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
+                await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
             }
         }
+    } else {
+        const reply = {
+            chat_id: message.chat.id,
+            text: 'Sorry, I didn\'t understand that. Type /help for more information'
+        };
+        await axios.post(`${TELEGRAM_API}/sendMessage`, reply);
     }
     res.send();
 });
